e44f0fbe41f4054f923961e153b9651648e0e8a6
SufianGfx <sufigfx@gmail.com>
2023-04-01 20:06:25 +0500

    Assignment completed

---
+++ app.js
@@ -0,0 +1,43 @@
+alert('E-Commerce Web Site');
+
+console.log("SufiGfx");
+
+var firstname = prompt("Enter First Name")
+var lastname = prompt("Enter Last Name")
+var city = prompt("Enter City", "Karachi")
+console.log(firstname + " " + lastname + " from " + city)
+
+var title = "SufiGfx Online Graphics";
+
+console.log("SufiGfx Online Graphics");
+
+var x = 10;
+var y = x + 20;
+
+console.log(y);
+
+var a = 10;
+var b = 20;
+var c = 40;
+var d = 50;
+var x = (a + b) * c + d;
+
+console.log(x);
+
+var a = "10"
+var b = 20
+
+var c = b + a
+
+console.log(c)
+
+var x = "Sufian "
+var y = "Shoaib"
+var z = x + y
+console.log(z)
+
+var a = 10;
+var b = a--
+console.log(b)
+console.log(a)
+
